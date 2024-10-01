b70fb2a642ef005fe77e62e4c0214d1b0606c8f8
hectorgarciatw <dreamallica@gmail.com>
2024-10-01 14:04:53 -0300

    Exercise 9.10 completed

--- src/types.ts
+++ src/types.ts
@@ -4,10 +4,28 @@ export type Visibility = "great" | "good" | "ok" | "poor";
 
 export type NonSensitiveDiaryEntry = Omit<DiaryEntry, "comment">;
 
+//Interface for entries
 export interface DiaryEntry {
     id: number;
     date: string;
     weather: Weather;
     visibility: Visibility;
     comment?: string;
+}
+
+// Interface for patients
+export interface Patient {
+    id: string;
+    name: string;
+    dateOfBirth: string;
+    ssn: string;
+    gender: "male" | "female" | "other";
+    occupation: string;
+}
+
+//Interface for diagnoses
+export interface Diagnose {
+    code: string;
+    name: string;
+    latin?: string;
 }
