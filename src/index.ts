3bc6d5d94fa604bae397f81467ba6ac0a4d3d37a
hectorgarciatw <dreamallica@gmail.com>
2024-10-04 15:51:22 -0300

    Exercise 9.20 completed

--- src/index.ts
+++ src/index.ts
@@ -19,5 +19,5 @@ app.use("/api/diaries", diaryRoutes);
 app.use("/api/patients", patientsRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on port http://localhost:${PORT}`);
 });
